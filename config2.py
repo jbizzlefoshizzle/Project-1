8b401224befbf95c2f4f4e8f56d3ed97477b1edc
Shaun Clarke <shaunclarke43@gmail.com>
2019-06-03 20:19:56 -0700

    map is almost done

---
+++ config2.py
@@ -0,0 +1,7 @@
+api_key="qQKFH7ON5ZMnxC1FlVf-uR4-lqBsw1Izi7G7bFaQoZZzs10TDoRkSMzZCF4fjhxmdv5ZyZ2hZTnM8f8HH5cLWApaL3Fn7C1Wp82PEkex4QsBO5LwZVsXRv__IjfvXHYx"
+
+token="YMS3nsDc5AUcix9rnsV4oQ"
+
+api_key2 = "AIzaSyAfq9IpX2PQ8XLWPKdO81iCV8f0aoH1dcc"
+
+gkey = "AIzaSyCOjS20hrigWbp9pozTNcAXmi4VJvGX4QQ"
